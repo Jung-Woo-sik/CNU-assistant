6c1c1b50fa327b2aa27802cde435e1b74300613a
Jung-Woo-sik <o0o0@cs-cnu.org>
2018-11-27 11:31:10 +0900

    assignment5

---
+++ Object_Oriented_Programing/assignment5/practice01_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int num1, num2;
+	cout << "input 2 number : ";
+	cin >> num1 >> num2;
+
+	try{
+		if(num2==0)
+			throw num2;
+		cout << "quotient : " << num1/num2 << endl;
+		cout << "remainder : " <<num1%num2 << endl;
+	}
+	catch(int expn){
+		cout << "The divisor can nat be" << expn <<"." << endl;
+		cout << "Please restat the program" << endl;
+	}
+	return 0;
+}
