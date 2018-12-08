d8a0432b773673d3f6aa10f9c590d37da383c98a
Jung-Woo-sik <o0o0@cs-cnu.org>
2018-12-08 18:39:48 +0900

    assignment 4

---
+++ Object_Oriented_Programing/assign4/MyArrayList.h
@@ -0,0 +1,50 @@
+#ifndef MYARRAYLIST_H
+#define MYARRAYLIST_H
+
+using namespace std;
+
+template < typename T>
+class MyArrayList {
+
+    public:
+        MyArrayList(){
+            mass = new T[DEFAULT_CAPACITY];
+            capacity = DEFAULT_CAPACITY;
+            size = 0;
+        }
+        MyArrayList(int x) {
+            if(x > 0)
+            {
+                capacity = x;
+                size = 0;
+                mass = new T[capacity];
+            }
+        }
+        bool addElement(T const &x) {
+            mass[size++] = x;
+            return true;
+        }
+        
+        void printIt() {
+            cout << " { ";
+            for(int i = 0; i < size; i++)
+            {
+                if(i == size -1 )
+                {
+                    cout << mass[i] << " }" << endl << endl;
+                    return;        
+                }
+                cout << mass[i] << ", ";
+            }
+        }
+
+
+    private:
+        const static int DEFAULT_CAPACITY = 10;
+        T * mass;
+        int capacity = 0;
+        int size = 0;
+
+};
+
+#endif
